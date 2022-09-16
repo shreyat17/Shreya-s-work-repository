eeb933f080dbd46e00d9b0f98b4f6bee720e26b8
shreyat17 <shreyatr178@gmail.com>
2022-09-16 01:40:05 +0100

    Assessment3

---
+++ Assessment3/WebApplication2/WebApplication2/WebForm2.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication2
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-LVRMSH9\\SQLEXPRESS;User id = Sa;password = shreyatr;initial catalog=SnadDB");
+            SqlCommand cmd = new SqlCommand("select * from[Employee data]", con);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+    }
+}
